f93a0327e262041b5323e2e816e30e79efd56002
marcosxavierdev <sis.marcosxavier@gmail.com>
2024-10-24 12:22:58 -0300

    first commit

---
+++ src/main/java/com/consulting/pismo_integration_service/PismoIntegrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.consulting.pismo_integration_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PismoIntegrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PismoIntegrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consulting/pismo_integration_service/PismoIntegrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consulting.pismo_integration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PismoIntegrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
